78cb6a4d9734976e775450fca568b3b1f53ff701
Patrick El-Azem <paelaz@microsoft.com>
2020-08-24 10:29:45 -0400

    Add Synapse notebooks to write raw data to ADLS2 and read scored data from ADLS2 and write it back to Cosmos DB

---
+++ IoT/deploy/deploy.synsasec.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# ====================
+# Variables
+
+subscription_id=""
+location=""
+resource_group_name=""
+
+template_file="deploy.synsasec.json"
+deployment_name="deploy_synsasec"
+
+# ====================
+
+az group deployment create --subscription "$subscription_id" \
+	-g "$resource_group_name" -n "$deployment_name" --template-file "$template_file" \
+	--parameters location="$location" --verbose
