a8b3d32c71049c36aa3b4ffc1550a88cb15c88d8
Jim Counts <jamesrcounts@outlook.com>
2022-01-25 19:47:23 -0800

    Update fleet data generator

--- IoT/Solution/FleetDataGenerator/DeviceManager.cs
+++ IoT/Solution/FleetDataGenerator/DeviceManager.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.Devices;
-using Microsoft.Azure.Devices.Client.Exceptions;
+using Microsoft.Azure.Devices.Common.Exceptions;
 
 namespace FleetDataGenerator
 {
